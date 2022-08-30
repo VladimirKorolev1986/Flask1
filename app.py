58662901b6a91350746cc56898c6c66d97f03287
VladimirKorolev1986 <theing1986@gmail.com>
2022-08-30 21:33:50 +0300

    settig for heroku

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, abort, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).parent
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{BASE_DIR / 'main.db'}"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("://", "ql://",
+                                                                               1) or f"sqlite:///{BASE_DIR / 'main.db'}"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
